28a8adb97ae3c71e23ff825393fa0a0da7e0775e
Ricky Thang <rthang11@yahoo.com>
2022-01-08 19:03:02 -0800

    refactor - app/models/user.py - changed hashed_password property to hashedPassword

--- app/models/user.py
+++ app/models/user.py
@@ -7,17 +7,19 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
+    #firstName
+    #lastName
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
-    hashed_password = db.Column(db.String(255), nullable=False)
+    hashedPassword = db.Column(db.String(255), nullable=False)
 
     @property
     def password(self):
-        return self.hashed_password
+        return self.hashedPassword
 
     @password.setter
     def password(self, password):
-        self.hashed_password = generate_password_hash(password)
+        self.hashedPassword = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
