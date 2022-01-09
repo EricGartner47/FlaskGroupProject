6344192f65103e0026f9e6b10e8f0630c21bd351
Ricky Thang <rthang11@yahoo.com>
2022-01-08 19:19:00 -0800

    refactor - app/seeds/users.py - reordered firstName and lastName arguments

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,11 +4,11 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password', firstName='Demo')
+        firstName='Demo', username='Demo', email='demo@aa.io', password='password')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password', firstName='Marnie', lastName='Moriyama')
+        firstName='Marnie', lastName='Moriyama', username='marnie', email='marnie@aa.io', password='password')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password', firstName='Robert', lastName ='De Niro')
+        firstName='Robert', lastName='De Niro', username='bobbie', email='bobbie@aa.io', password='password')
 
     db.session.add(demo)
     db.session.add(marnie)
