1d4819df5bc3cedf4683fbea534d0df43caee14e
Joe Yang <josephjyang@gmail.com>
2022-01-10 08:39:18 -0800

    added completed date field

--- app/models/task.py
+++ app/models/task.py
@@ -9,6 +9,7 @@ class Task(db.Model):
     notes = db.Column(db.Text)
     due_date = db.Column(db.DateTime)
     completed = db.Column(db.Boolean, default=False)
+    completed_date = db.Column(db.DateTime)
     list_id = db.Column(db.Integer, db.ForeignKey("lists.id"))
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     list = db.relationship("List", back_populates="tasks")
@@ -21,6 +22,7 @@ class Task(db.Model):
             'notes': self.notes,
             'due_date': self.due_date,
             'completed': self.completed,
+            'completed_date': self.completed_date,
             'list_id': self.list_id,
             'user_id': self.user_id,
         }
