12dd12fa9a3dbc2b5617c02bbbe6edcc987cfa03
Joe Yang <josephjyang@gmail.com>
2022-01-10 10:20:20 -0800

    feature - homepage navbar - updated navlinks, fonts, background color, link hover behavior, etc

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,34 +2,35 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import './NavBar.css'
 
 const NavBar = () => {
   return (
     <nav>
+      <div id="logo">
+        <NavLink to='/' exact={true} activeClassName='active'>
+          Home
+        </NavLink>
+      </div>
       <ul>
         <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
           <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
+            Log in
           </NavLink>
         </li>
         <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
+          <NavLink id="signup" to='/sign-up' exact={true} activeClassName='active'>
+            Sign up for free
           </NavLink>
         </li>
-        <li>
+        {/* <li>
           <NavLink to='/users' exact={true} activeClassName='active'>
             Users
           </NavLink>
         </li>
         <li>
           <LogoutButton />
-        </li>
+        </li> */}
       </ul>
     </nav>
   );
