e9da08502d75ee29c001f2ed8242ace092a24a17
EricGartner47 <egartner47@gmail.com>
2022-01-11 10:58:04 -0500

    feature - list route - started create list route

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
+from app.forms.list_form import NewList
 from app.models import User, Task, db, List
-from app.forms import NewTask
+from app.forms import NewTask, NewList
 
 user_routes = Blueprint('users', __name__)
 
@@ -36,6 +37,7 @@ def create_task(id):
         form.populate_obj(task)
         db.session.add(task)
         db.session.commit()
+# not finished
 
 @user_routes.route('/<int:id>/lists')
 @login_required
@@ -43,3 +45,15 @@ def get_all_lists(id):
     user = User.query.get(id)
     results = List.query.filter(List.user_id == user.id).all()
     return {'lists': [list.to_dict() for list in results]}
+
+@user_routes.route('/<int:id>/lists', methods=['POST'])
+@login_required
+def create_list(id):
+    user = User.query.get(id)
+    form = NewList()
+    if form.validate_on_submit():
+        list = List()
+        form.populate_obj(list)
+        db.session.add(list)
+        db.session.commit()
+# not finished 
