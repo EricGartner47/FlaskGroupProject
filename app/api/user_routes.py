0c1a851105916ac9165927cdbc1b0e15b687f25f
EricGartner47 <egartner47@gmail.com>
2022-01-10 20:06:11 -0500

    feature - api route - finished get tasks route

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import User
+from app.models import User, Task
 
 user_routes = Blueprint('users', __name__)
 
@@ -17,3 +17,21 @@ def users():
 def user(id):
     user = User.query.get(id)
     return user.to_dict()
+
+@user_routes.route('/<int:id>/tasks')
+@login_required
+def get_all_tasks(id):
+    user = User.query.get(id)
+    results = Task.query.filter(Task.user_id == user.id).all()
+    return {'tasks': [task.to_dict() for task in results]}
+
+@user_routes.route('/<int:id>/tasks', methods=['POST'])
+@login_required
+def create_task(id):
+    user = User.query.get(id)
+    form = Task()
+    # if form.validate_on_submit():
+    #     = ()
+    #     form.populate_obj()
+    #     db.session.add()
+    #     db.session.commit()
