5137ffab3b06d129121f201898c6a952e8b5b889
Ricky Thang <rthang11@yahoo.com>
2022-01-13 10:48:19 -0800

    style - Task Form Update - space out rows

--- react-app/src/components/TaskFormUpdate/index.js
+++ react-app/src/components/TaskFormUpdate/index.js
@@ -89,43 +89,73 @@ const TaskFormUpdate = ({task}) => {
                     {errors.map((error, idx) => <li key={idx}>{error}</li>)}
                 </ul>
                 <form onSubmit={handleSubmit}>
-                    <input
-                        id="task-name"
-                        type='text'
-                        placeholder='Add a Task...'
-                        value={taskName}
-                        onChange={e => setTaskName(e.target.value)}
-                    />
-                    <textarea
-                        id="task-notes"
-                        value={notes}
-                        onChange={e => setNotes(e.target.value)}
-                        rows={3}
-                        cols={5}
-                        placeholder="Add a note..."
-                    />
-                    <input
-                        id="task-due-date"
-                        type='date'
-                        value={dueDate}
-                        onChange={e => setDueDate(e.target.value)}
-                    />
-                    <input
-                        id="task-completed"
-                        type='checkbox'
-                        value={completed}
-                        onChange={e => setCompleted(!completed)}
-                    />
-                    <select
-                        id="list-select"
-                        value={list || "select"}
-                        onChange={e => { setList(e.target.value) }}
-                    >
-                        <option value={"select"}>Select a notebook</option>
-                        {userLists.map(list => {
-                            return <option key={list.id} value={list.id}>{list.name}</option>
-                        })}
-                    </select>
+
+                    <div class="input-row big" id="input-task-name">
+                        <label for="task-name" id="label-task-name">Name Your Task</label>
+                        <input
+                            id="task-name"
+                            name="task-name"
+                            type='text'
+                            placeholder='Add a Task...'
+                            value={taskName}
+                            onChange={e => setTaskName(e.target.value)}
+                        />
+                    </div>
+
+                    <div class="input-row small">
+                        <label for="task-due-date">due</label>
+                        <input
+                            id="task-due-date"
+                            name="task-due-date"
+                            type='date'
+                            value={dueDate}
+                            onChange={e => setDueDate(e.target.value)}
+                        />
+                    </div>
+
+                    <div class="input-row small">
+                        <label for="list-select">list</label>
+                        <select
+                            id="list-select"
+                            name="list-select"
+                            value={list || "select"}
+                            onChange={e => { setList(e.target.value) }}
+                        >
+                            <option value={"select"}>Select a notebook</option>
+                            {userLists.map(list => {
+                                return <option key={list.id} value={list.id}>{list.name}</option>
+                            })}
+                        </select>
+                    </div>
+
+                    <div class="input-row small">
+                        <label for="task-completed">complete</label>
+                        <input
+                            id="task-completed"
+                            name="task-completed"
+                            type='checkbox'
+                            value={completed}
+                            onChange={e => setCompleted(!completed)}
+                        />
+                    </div>
+
+                    <div class="notes" id="input-notes">
+                        <label for="task-notes" id="label-notes">Notes</label>
+                        <br/>
+                        <textarea
+                            id="task-notes"
+                            name="task-notes"
+                            value={notes}
+                            onChange={e => setNotes(e.target.value)}
+                            rows={3}
+                            cols={5}
+                            placeholder="Add a note..."
+                        />
+                    </div>
+                    
+                    
+                    
+                    
                     <button type='submit'>Update Task</button>
                 </form>
             </div>
