5653e1c0f1f1b80549f7cc9e3748f765f946807b
Joe Yang <josephjyang@gmail.com>
2022-01-15 00:12:11 -0800

    feature - home navbar - css styling

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -6,7 +6,7 @@ import './NavBar.css'
 
 const NavBar = () => {
   return (
-    <nav>
+    <nav id="homepage-nav">
       <div id="ymtl-container-home">
         <NavLink to='/' exact={true} activeClassName='active' className="ymtl-logo">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Notepad_icon.svg/2048px-Notepad_icon.svg.png" alt="" id="notepad"></img>
